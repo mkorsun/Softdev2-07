f8233e180bbb2f11f3b2872e08c179dc2c43ddc4
Max Korsun <mkorsun@stuy.edu>
2018-02-28 22:16:50 -0500

    done

--- svg.js
+++ svg.js
@@ -1,32 +1,45 @@
 
 var c = document.getElementById("svg_id");
 
-
-var clears = function() {
+var clears = function(e) {
     while (c.hasChildNodes()){
 	c.removeChild(c.firstChild)
     }
+    prevx = -1;
+    prevy = -1;
 }
 
-var cbutton = clear.addEventListener("click", clears);
 
 
+var prevx = -1;
+var prevy = -1;
 
 var draw = function(e) {
     var x= e.offsetX;
-    var y= e.offestY; 
-    var r = 1;
+    var y= e.offsetY; 
+    var circle = document.createElementNS("http://www.w3.org/2000/svg", "circle")
+    circle.setAttribute("cx",x+"");
+    circle.setAttribute("cy",y+"");
+    circle.setAttribute("r",25+"");
+    circle.setAttribute("fill","black");
+    circle.setAttribute("stroke","black");
+    c.appendChild(circle)
+    if(prevx != -1){
+	var line =  document.createElementNS("http://www.w3.org/2000/svg", "line")
+	line.setAttribute("x1",x+"");
+	line.setAttribute("x2",prevx+"");
+	line.setAttribute("y1",y+"");
+	line.setAttribute("y2",prevy+"");
+	line.setAttribute("stroke","black");
+	line.setAttribute("stroke-width","2");
+	c.appendChild(line);
+    }
+    prevx = x;
+    prevy = y;
    
 }
-var start = function(){
-    window.requestAnimationFrame(animate);
-}
 
-startB.addEventListener("click",start);
-var stops = function(e) {
-    window.cancelAnimationFrame(requestID);
 
-}
-var stop = document.getElementById("stop");
-stop.addEventListener("click",stops);
+c.addEventListener("click", draw);
 
+clear.addEventListener("click", clears);
